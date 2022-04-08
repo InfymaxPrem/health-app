2bf54464dde38c5424664e63d1ae5faf6fff3e5e
rounak-dev <rounak.infymax@gmail.com>
2022-04-08 13:02:40 +0530

    controlles and model added

--- backend/server.js
+++ backend/server.js
@@ -1,44 +1,21 @@
 const express = require('express');
 require('dotenv').config();
+const friendsController = require('./controllers/friends.controller');
+const messagesController = require('./controllers/messages.controller');
 
 const app = express();
 
 const port = process.env.PORT;
 
-const friends = [
-  {
-    id: 0,
-    name: 'Albert Einstein'
-  },
-  {
-    id: 1,
-    name: 'Sir Isaac Newton'
-  }
-];
+app.use(express.json());
 
-app.get('/friends', (req, res) => {
-  res.json(friends);
-});
+app.post('/friends', friendsController.postFriend);
+app.get('/friends', friendsController.getFriends);
+app.get('/friends/:friendId', friendsController.getFriend);
 
-app.get('/friends/:friendId', (req, res) => {
-  const friendId = Number(req.params.friendId);
-  const friend = friends[friendId];
-  if (friend) {
-    res.status(200).json(friend);
-  } else {
-    res.status(404).json({
-      error: "Friend does not exist"
-    });
-  }
-});
+app.get('/messages', messagesController.getMessages);
+app.post('/messages', messagesController.postMessage);
 
-app.get('/messages', (req, res) => {
-  res.send('<ul><li>Helloo Albert!</li></ul>');
-});
-
-app.post('/messages', (req, res) => {
-  console.log('Updating messages...');
-});
 
 app.listen(port, () => {
   console.log(`Listening on ${port}...`);
