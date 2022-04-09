9907ca95c7662f3dd1edaba4121e2f3f0652f91b
rounak-dev <rounak.infymax@gmail.com>
2022-04-09 12:11:42 +0530

    database connection successful

--- backend/app.js
+++ backend/app.js
@@ -1,11 +1,17 @@
 const express = require('express');
+const cors = require('cors');
+
+
 
 const friendsRouter = require('./routes/friends.router');
 const messagesRouter = require('./routes/messages.router');
 
 const app = express();
 
+
 app.use(express.json());
+
+
 
 app.use('/friends', friendsRouter);
 app.use('/messages', messagesRouter);
--- backend/server.js
+++ backend/server.js
@@ -1,9 +1,23 @@
 const http = require('http');
+const mysql = require('mysql2');
 require('dotenv').config();
 
 const app = require('./app');
 
 const port = process.env.PORT;
+
+const db = mysql.createPool({
+  connectionLimit: 10,
+  host: process.env.DB_HOST,
+  user: process.env.DB_USER,   
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB,
+})
+
+db.getConnection( (err, connection)=> {
+  if (err) throw (err)
+  console.log ("DB connected successful: ")
+})
 
 const server = http.createServer(app);
 
